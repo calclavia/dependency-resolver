d20204e1eba045e6de97421ff9880ab10173c8a5
Henry Mao <Henry Mao>
2016-10-12 11:24:49 -0700

    Defined parseInput function contract and test case

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * Contains all functions to execute the program
+ */
+module.export = new class {
+    /**
+     * Given a list of strings that contain a a package and its
+     * dependency, return an object that maps package keys to
+     * their dependecy values.
+     * 
+     * Example:
+     * 
+     * input: ['KittenService: CamelCaser', 'CamelCaser: ']
+     * output: {'KittenService': 'CamelCaser'}
+     * 
+     * @param {Array} dependencies - An array of package and their dependencies.
+     *                               The array is non-nullable.
+     * @return A JSON object that sparsely maps each package key to its dependency.
+     *         If a package does not have a dependency, it will not have a key.
+     */
+    parseInput(dependencies) {
+
+    }
+}
---
+++ test.js
@@ -0,0 +1,14 @@
+const index = require('index')
+const assert = require('assert')
+
+describe('Dependency resolver', () => {
+  describe('#parseInput()', () => {
+    it('should return an object that maps package keys to their dependecy', () => {
+      
+    })
+
+    it('should return an object that does not contain keys for packages without dependencies', () => {
+      
+    })
+  })
+})
