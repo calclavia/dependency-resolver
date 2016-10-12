82663a2e5686ababc12f25df8ccc4ce1fd1eb0f9
Henry Mao <Henry Mao>
2016-10-12 11:33:26 -0700

    Added .editorconfig file for formatting

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
  * Contains all functions to execute the program
  */
 module.export = {
-    /**
-     * Given a list of strings that contain a a package and its
-     * dependency, return an object that maps package keys to
-     * their dependecy values.
-     * 
-     * Example:
-     * 
-     * input: ['KittenService: CamelCaser', 'CamelCaser: ']
-     * output: {'KittenService': 'CamelCaser'}
-     * 
-     * @param {Array} dependencies - An array of package and their dependencies.
-     *                               The array is non-nullable.
-     * @return A JSON object that sparsely maps each package key to its dependency.
-     *         If a package does not have a dependency, it will not have a key.
-     */
-    parseInput(dependencies) {
-        return {}
-    }
+  /**
+   * Given a list of strings that contain a a package and its
+   * dependency, return an object that maps package keys to
+   * their dependecy values.
+   * 
+   * Example:
+   * 
+   * input: ['KittenService: CamelCaser', 'CamelCaser: ']
+   * output: {'KittenService': 'CamelCaser'}
+   * 
+   * @param {Array} dependencies - An array of package and their dependencies.
+   *                               The array is non-nullable.
+   * @return A JSON object that sparsely maps each package key to its dependency.
+   *         If a package does not have a dependency, it will not have a key.
+   */
+  parseInput(dependencies) {
+    return {}
+  }
 }
